7cf672e4ba881186401ba0222725dbda8453af95
Nate Woods <Nate.Woods@workiva.com>
2016-09-20 05:26:57 -0600

    Moving to real settings implementation

--- web/main.dart
+++ web/main.dart
@@ -1,50 +1,128 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:convert';
 
 // TODO: favicon http://stackoverflow.com/questions/260857/changing-website-favicon-dynamically
 
-// TODO: load these from localstorage
-Map<String, List<String>> services = {
-  "trans": ["translate", "/health"],
-  "lux": ["docsserver", "/api/v1/health"],
-};
+/*
+{
+  "svcz": {
+    "trans": "translate.$domain/health",
+    "lux": "docsserver.$domain/api/v1/health"
+  },
+  "envz": {
+    "dev": "wk-dev.wdesk.org",
+    "stage": "sandbox.wdesk.com",
+    "prod": "app.wdesk.com"
+  },
+  "skip": ["lux-prod"]
+}
+ */
 
-Map<String, String> envs = {
-  "dev": "wk-dev.wdesk.org",
-  "stage": "sandbox.wdesk.com",
-  "prod": "app.wdesk.com",
-};
+class Settings {
+  static final Settings _singleton = new Settings._internal();
+  factory Settings() => _singleton;
 
-Set<String> skip = new Set<String>()..add('lux-prod');
+  Map<String, String> svcz = new Map<String, String>();
+  Map<String, String> envz = new Map<String, String>();
+  Set<String> skip = new Set<String>();
+  HtmlElement dialog, open, close, save;
+  TextAreaElement input;
+  JsonEncoder encoder;
+  StreamController updates = new StreamController.broadcast();
+
+  Settings._internal() {
+    open = new ButtonElement()
+      ..classes.addAll(['click', 'open'])
+      ..setInnerHtml('&#x2699;')
+      ..onClick.listen(openFn);
+    close = new ButtonElement()
+      ..classes.addAll(['click', 'close'])
+      ..setInnerHtml('&times;')
+      ..onClick.listen(closeFn);
+    save = new ButtonElement()
+      ..classes.addAll(['click', 'save'])
+      ..setInnerHtml('&#x1f4be;')
+      ..onClick.listen(saveFn);
+    input = new TextAreaElement();
+    dialog = new DivElement()
+      ..classes.addAll(['cover', 'hide'])
+      ..append(
+        new DivElement()
+          ..classes.add('cover-content')
+          ..append(close)
+          ..append(save)
+          ..append(input)
+      );
+    document.body.append(dialog);
+    document.body.append(open);
+
+    // Setup JSON parser + initilize with localStorage data
+    encoder = new JsonEncoder.withIndent(' ');
+    if (window.localStorage.containsKey('status'))
+      assign(window.localStorage['status']);
+  }
+
+  assign(String blob, {bool again: false}) {
+    var before = json();
+    try {
+      Map optz = JSON.decode(blob);
+      envz = optz['envz'];
+      svcz = optz['svcz'];
+      List s = optz['skip'];
+      skip = s.toSet();
+      window.localStorage['status'] = JSON.encode(json());
+      updates.add('updated');
+    } catch (e, t) {
+      String suffix = again ? '(double-fail)' : '(reverting)';
+      print('Problem Parsing Settings ' + suffix);
+      print(e);
+      print(t);
+      if (!again) assign(JSON.encode(before), again: true);
+    }
+  }
+
+  void openFn(MouseEvent e) {
+    input.value = encoder.convert(json());
+    dialog.classes.remove('hide');
+  }
+
+  void saveFn(MouseEvent e) {
+    assign(input.value);
+    closeFn(e);
+  }
+
+  Map json() => {'envz': envz, 'svcz': svcz, 'skip': skip.toList()};
+  closeFn(MouseEvent e) => dialog.classes.add('hide');
+  Stream get onChange => updates.stream;
+}
 
 void main() {
+  Settings options = new Settings();
   DivElement container = document.getElementById("container");
-
   TableElement table = new TableElement();
+  container.children.add(table);
+  options.onChange.listen((e) => draw(table, options));
+  draw(table, options);
+}
+
+draw(TableElement table, Settings optz) {
+  table.setInnerHtml(''); // clear
   TableRowElement thead = table.createTHead().addRow();
   thead.addCell(); // empty corner
-  for (var env in envs.keys) {
+  for (var env in optz.envz.keys) {
     thead.addCell().text = env;
   }
   TableSectionElement tbody = table.createTBody();
-  for (var key in services.keys) {
-    createRow(tbody.addRow(), key);
+  for (var key in optz.svcz.keys) {
+    createRow(optz, tbody.addRow(), key);
   }
-  container.children.add(table);
-
-  // Storage store = window.localStorage;
-  // if (store.containsKey("status")) {
-  //   print("yep");
-  // } else {
-  //   print("nope");
-  // }
-  // store["status"] = "here";
 }
 
-createRow(TableRowElement row, String svc) {
+createRow(Settings optz, TableRowElement row, String svc) {
   row.addCell().text = svc;
-  for (var env in envs.keys) {
-    row.addCell().append(new StatusElement(env, svc));
+  for (var env in optz.envz.keys) {
+    row.addCell().append(new StatusElement(optz, env, svc));
     // row.addCell().append(new Status(env, svc));
   }
 }
@@ -56,7 +134,7 @@ class StatusElement extends DivElement {
     print("Status Created");
   }
 
-  factory StatusElement(String env, String svc) {
+  factory StatusElement(Settings optz, String env, String svc) {
     var spot = new DivElement();
     var load = new DivElement();
 
@@ -64,7 +142,7 @@ class StatusElement extends DivElement {
       ..append(spot)
       ..append(load);
 
-    new Status(spot, env, svc);
+    new Status(optz, spot, env, svc);
 
     return obj;
   }
@@ -75,13 +153,12 @@ class Status {
   String target;
   Timer last;
 
-  Status(this.obj, String env, String svc) {
+  Status(Settings optz, this.obj, String env, String svc) {
     obj.classes.add('status');
     obj.onClick.listen((e) => run());
-    var s = services[svc];
-    this.target = "https://" + s[0] + "." + envs[env] + s[1];
+    this.target = "https://" + optz.svcz[svc].replaceFirst("\$domain", optz.envz[env]);
 
-    if (skip.contains(env + "-" + svc) || skip.contains(svc + "-" + env)) {
+    if (optz.skip.contains(env + "-" + svc) || optz.skip.contains(svc + "-" + env)) {
       obj.classes.add('status-ignore');
     } else {
       obj.classes.add('status-loading');
