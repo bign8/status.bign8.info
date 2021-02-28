105e938d5174d76a53edb267ec3dc62fbc1f44e6
Nate Woods <big.nate.w@gmail.com>
2021-02-27 22:34:57 -0600

    refactor(ship): break complicated logic into stand-alone section :recycle:

--- ship.sh
+++ ship.sh
@@ -1,8 +1,18 @@
 #! /bin/bash -ex
 
+case $1 in
+  dart)
+    dart --disable-analytics
+    dart pub get
+    dart pub run build_runner build
+    dart compile js -m -o web/static/main.dart.js web/dart/main.dart
+    exit 0
+    ;;
+esac
+
 DKR="docker run --rm -it -w /mnt -v $PWD:/mnt"
 $DKR xzyfer/docker-libsass:3.2.5 --style compressed -m web/sass/style.sass web/static/style.css
-$DKR google/dart:2 bash -xc "pub get && pub run build_runner build && dart compile js -m -o web/static/main.dart.js web/dart/main.dart"
+$DKR google/dart:2 ./ship.sh dart
 go build -o status -ldflags="-s -w" -v .
 gzip -f status
 scp status.gz me.bign8.info:/opt/bign8
